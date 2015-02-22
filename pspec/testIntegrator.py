3e3b37d578bac3d10cb0357cd3a100610d435b4f
Joy M. Monteiro <joy.merwin@gmail.com>
2015-02-22 10:24:32 +0530

    test script for RungeKutta

---
+++ pspec/testIntegrator.py
@@ -0,0 +1,23 @@
+from pylab import *;
+import RungeKutta;
+
+def dummy(t, f, args):
+
+    return zeros(f.shape);
+
+def dummyVel(f, args):
+
+    return 1.3;
+
+u = zeros((10,10));
+v = zeros((10,10));
+z = zeros((10,10));
+
+delta = 0.1;
+
+
+stepfwd = RungeKutta.RungeKutta4(delta, dummy, dummy, dummyVel);
+
+tnew, fnew = stepfwd.integrate(0, [u,v,z]);
+
+print tnew;
