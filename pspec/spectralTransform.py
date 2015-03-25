2bec5b16abb9c7b28f2eec130b15f087b2b6c384
suhasdl <suhasdl.mysore@gmail.com>
2015-03-25 12:31:35 +0530

    Some minor changes.

--- pspec/spectralTransform.py
+++ pspec/spectralTransform.py
@@ -19,19 +19,19 @@ class specTrans(object):
         self.yType = yType;
 
         self.inpArr = pyfftw.n_byte_align_empty(\
-                (numPointsX, numPointsY),\
+                (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
                 dtype='float64');
 
         self.outArr = pyfftw.n_byte_align_empty(\
-                (numPointsX, numPointsY),\
+                (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
                 dtype='float64');
 
         if (xType is 'Fourier'):
 
             self.interxArr = pyfftw.n_byte_align_empty(\
-                    (numPointsX/2 + 1,numPointsY),\
+                    (numPointsY, numPointsX/2 + 1),\
                     pyfftw.simd_alignment,
                     dtype='complex128');
         else:
@@ -43,7 +43,7 @@ class specTrans(object):
         if (yType is 'Fourier'):
 
             self.interyArr = pyfftw.n_byte_align_empty(\
-                    (numPointsX, numPointsY/2 + 1),\
+                    (numPointsY/2 + 1, numPointsX),\
                     pyfftw.simd_alignment,
                     dtype='complex128');
 
@@ -80,11 +80,11 @@ class specTrans(object):
             self.isOneDimensional = True;
 
             self.fwdxTrans = pyfftw.FFTW(\
-                self.inpArr, self.interxArr, axes=[0,],\
+                self.inpArr, self.interxArr, axes=[1,],\
                 flags=['FFTW_PATIENT',],threads=self.numCpus);
 
             self.invxTrans = pyfftw.FFTW(\
-                self.interxArr, self.outArr, axes=[0,],\
+                self.interxArr, self.outArr, axes=[1,],\
                 direction='FFTW_BACKWARD',
                 flags=['FFTW_PATIENT',],threads=self.numCpus);
 
@@ -95,22 +95,22 @@ class specTrans(object):
             if (xType is 'Fourier'):
 
                 self.fwdxTrans = pyfftw.FFTW(\
-                    self.inpArr, self.interxArr, axes=[0,],\
+                    self.inpArr, self.interxArr, axes=[1,],\
                     flags=['FFTW_PATIENT',],threads=self.numCpus);
 
                 self.invxTrans = pyfftw.FFTW(\
-                    self.interxArr, self.outArr, axes=[0,],\
+                    self.interxArr, self.outArr, axes=[1,],\
                     direction='FFTW_BACKWARD',
                     flags=['FFTW_PATIENT',],threads=self.numCpus);
             
             if (yType is 'Fourier'):
 
                 self.fwdyTrans = pyfftw.FFTW(\
-                    self.inpArr, self.interyArr, axes=[1,],\
+                    self.inpArr, self.interyArr, axes=[0,],\
                     flags=['FFTW_PATIENT',],threads=self.numCpus);
 
                 self.invyTrans = pyfftw.FFTW(\
-                    self.interyArr, self.outArr, axes=[1,],\
+                    self.interyArr, self.outArr, axes=[0,],\
                     direction='FFTW_BACKWARD',
                     flags=['FFTW_PATIENT',],threads=self.numCpus);
 
@@ -140,16 +140,16 @@ class specTrans2d(object):
         self.yType = yType;
 
         self.inpArr = pyfftw.n_byte_align_empty(\
-                (numPointsX, numPointsY),\
+                (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
                 dtype='complex128');
 
         self.outArr = pyfftw.n_byte_align_empty(\
-                (numPointsX, numPointsY),\
+                (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
                 dtype='complex128');
 
-        fname = str(numPointsX)+'x'+str(numPointsY)+'.wis'
+        fname = str(numPointsX)+'x'+str(numPointsY)+'_2d'+'.wis'
 
 # Check if wisdom files exist for this combination
 
