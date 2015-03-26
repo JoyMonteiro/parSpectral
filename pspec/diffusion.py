575506e82a4ce2ff75fe5cadeafeddfd33a355bb
suhasdl <suhasdl.mysore@gmail.com>
2015-03-26 18:25:04 +0530

    Indentation fixed.

--- pspec/diffusion.py
+++ pspec/diffusion.py
@@ -1,23 +1,25 @@
 from numpy import arange, pi, zeros, exp
 from scipy import *
 from spectralTransform import specTrans2d;
+import inversion;
+
 
 class specDiffusion(object):
     
-    def __init__(self, numPointsX, numPointsY,alpha, nu, 
-	    order = 8., length = 2*pi, xType='Fourier', yType='Fourier'):
+    def __init__(self, numPointsX, numPointsY,alpha, nu,  \
+        order = 8., length = 2*pi, xType='Fourier', yType='Fourier'):
         # nu is the higher "order" dissipation coefficient
         # alpha is the linear drag
 
         self.xn = numPointsX;
-	self.yn = numPointsY;
-	self.xType = xType;
-	self.yType = yType;
-	self.alpha = alpha;
-	self.nu = nu;
-	self.order = order;
+        self.yn = numPointsY;
+        self.xType = xType;
+        self.yType = yType;
+        self.alpha = alpha;
+        self.nu = nu;
+        self.order = order;
 
-	self.trans = specTrans2d(numPointsX, numPointsY, xType, yType);
+        self.trans = specTrans2d(numPointsX, numPointsY, xType, yType);
 
         #Prepare the wavenumber arrays
         self.kxx = (2*pi/length)*concatenate((arange(0,numPointsX/2),arange(-numPointsX/2,0)));
@@ -27,11 +29,11 @@ class specDiffusion(object):
 
 
         [kx,ky] = meshgrid(self.kxx,self.kyy);
-	self.trans.fwdTrans(field);
-	temp = self.trans.outArr;
-	
-	temp = temp * exp(-(self.nu*(kx**self.order+ky**self.order) + self.alpha) * dt)
-
-	self.trans.invTrans(temp);
-	return self.trans.outArr.real.copy();
-        
+    	self.trans.fwdTrans(field);
+    	temp = self.trans.outArr;
+    	
+    	temp = temp * exp(-(self.nu*(kx**self.order+ky**self.order) + self.alpha) * dt)
+    
+    	self.trans.invTrans(temp);
+    	return self.trans.outArr.real.copy();
+            
