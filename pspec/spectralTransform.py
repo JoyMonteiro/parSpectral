4a4419824db87becdb727935f70a7a75339c2399
suhasdl <suhasdl.mysore@gmail.com>
2015-04-02 21:22:22 +0530

    Added Chebyshev.

--- pspec/spectralTransform.py
+++ pspec/spectralTransform.py
@@ -18,6 +18,7 @@ class specTrans(object):
         self.xType = xType;
         self.yType = yType;
 
+
         self.inpArr = pyfftw.n_byte_align_empty(\
                 (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
@@ -27,6 +28,7 @@ class specTrans(object):
                 (numPointsY, numPointsX),\
                 pyfftw.simd_alignment,
                 dtype='float64');
+
 
         if (xType is 'Fourier'):
 
@@ -46,15 +48,28 @@ class specTrans(object):
                     (numPointsY/2 + 1, numPointsX),\
                     pyfftw.simd_alignment,
                     dtype='complex128');
-
+ 
+            fname = str(numPointsX)+'x'+str(numPointsY)+'.wis'
+        
         elif (yType is 'Cosine'):
 
-            print 'Non-Fourier options not (yet) implemented\
-               along y-axis!';
-            sys.exit();
+            self.inpCbyArr = pyfftw.n_byte_align_empty(\
+                    (2*numPointsY - 2, numPointsX),\
+                    pyfftw.simd_alignment,
+                    dtype='float64');
+
+            self.outCbyArr = pyfftw.n_byte_align_empty(\
+                    (2*numPointsY - 2, numPointsX),\
+                    pyfftw.simd_alignment,
+                    dtype='float64');
+
+            self.interCbyArr = pyfftw.n_byte_align_empty(\
+                    (numPointsY, numPointsX),\
+                    pyfftw.simd_alignment,
+                    dtype='complex128');
 
 
-        fname = str(numPointsX)+'x'+str(numPointsY)+'.wis'
+            fname = str(numPointsX)+'x'+str(numPointsY)+'_cby'+'.wis'
 
 # Check if wisdom files exist for this combination
 
@@ -114,6 +129,16 @@ class specTrans(object):
                     direction='FFTW_BACKWARD',
                     flags=['FFTW_PATIENT',],threads=self.numCpus);
 
+            elif (yType is 'Cosine'):
+
+                self.fwdyTrans = pyfftw.FFTW(\
+                    self.inpCbyArr, self.interCbyArr, axes=[0,],\
+                    flags=['FFTW_PATIENT',],threads=self.numCpus);
+
+                self.invyTrans = pyfftw.FFTW(\
+                    self.interCbyArr, self.outCbyArr, axes=[0,],\
+                    direction='FFTW_BACKWARD',
+                    flags=['FFTW_PATIENT',],threads=self.numCpus);
 
         print 'Done estimating!';
 
