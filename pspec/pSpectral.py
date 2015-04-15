d95a317e266632e09c7b667f6967b6b4b561cda7
suhasdl <suhasdl.mysore@gmail.com>
2015-04-15 10:56:19 +0530

    Some minor changes

--- pspec/pSpectral.py
+++ pspec/pSpectral.py
@@ -1,103 +1,118 @@
-import numpy as np;
-from numpy import arange, pi, zeros, exp, cos;
-from spectralTransform import specTrans;
-from numpy import vstack, transpose, newaxis, linspace, sum;
-
-#from numpy.fft.helper import ifftshift;
-from numpy.fft import rfft, irfft, fft2,ifft2;
+import numpy as np
+from numpy import arange, pi, zeros, exp, cos, ones
+from spectralTransform import specTrans
+from numpy import vstack, transpose, newaxis, linspace, sum
+from numpy import hstack, dot, eye, tile, diag
+#from numpy.fft.helper import ifftshift
+from numpy.linalg import matrix_power
+from numpy.fft import rfft, irfft, fft2,ifft2
 
 class parSpectral(object):
 
-    def __init__(self, numPointsX, numPointsY, length=2*pi,
-                xType='Fourier', yType='Fourier'):
+    def __init__(self, numPointsX, numPointsY, lengthX = 2*pi, lengthY = 2*pi , xType='Fourier', yType='Fourier'):
 
-        self.xn = numPointsX;
-        self.yn = numPointsY;
+        self.xn = numPointsX
+        self.yn = numPointsY
 
-        self.trans = specTrans(numPointsX, numPointsY, xType, yType);
+        self.trans = specTrans(numPointsX, numPointsY, xType, yType)
 
         #Prepare the wavenumber arrays
-        self.kx = 2*pi*1j*(arange(numPointsX/2+1))/length;
-        self.ky = 2*pi*1j*(arange(numPointsY/2+1))/length;
+        self.kx = 2*pi*1j*(arange(numPointsX/2+1))/lengthX
+        self.ky = 2*pi*1j*(arange(numPointsY/2+1))/lengthY
 
-        self.cby = cos(linspace(0,pi, numPointsY));
+        self.cby = cos(linspace(0,pi, numPointsY))
+        self.lengthY = lengthY
 
         #Prepare the filters
 
-        preserveX = self.xn/3;
-        truncateX = self.xn/2 - preserveX;
+        preserveX = self.xn/3
+        truncateX = self.xn/2 - preserveX
 
-        filterX = zeros((1, self.xn/2+1));
-        filterX[0, 0:preserveX] = 1.;
+        filterX = zeros((1, self.xn/2+1))
+        filterX[0, 0:preserveX] = 1.
 
-        i = arange(preserveX, self.xn/2);
-        filterX[0, i] = exp((preserveX-i)/1.);
+        i = arange(preserveX, self.xn/2)
+        filterX[0, i] = exp((preserveX-i)/1.)
 
-        self.filterX = filterX;
+        self.filterX = filterX
 
  
-        preserveY = self.yn/3;
-        truncateY = self.yn/2 - preserveY;
+        preserveY = self.yn/3
+        truncateY = self.yn/2 - preserveY
 
-        filterY = zeros((self.yn/2+1, 1));
-        filterY[0:preserveY, 0] = 1.;
+        filterY = zeros((self.yn/2+1, 1))
+        filterY[0:preserveY, 0] = 1.
 
-        i = arange(preserveY, self.yn/2);
-        filterY[i, 0] = exp((preserveY-i)/1.);
+        i = arange(preserveY, self.yn/2)
+        filterY[i, 0] = exp((preserveY-i)/1.)
 
-        self.filterY = filterY;
+        self.filterY = filterY
+        
+        def chebMatrix(N):
+            m = arange(0,N)
+            c = (hstack(( [2.], ones(N-2), [2.]))*(-1)**m).reshape(N,1)
+            X = tile(cos(pi*m/(N-1)).reshape(N,1),(1,N))
+            dX=X-(X.T)
+            D=dot(c,1./c.T)/(dX+eye(N))
+            D -= diag(sum(D.T,axis=0))
+            return D 
+
+        D = chebMatrix(numPointsY)
+        D[0,:] = np.zeros(self.yn)
+        D[-1,:] = np.zeros(self.yn)
+        self.D = D/(self.lengthY/2.)
 
 
     def partialX(self,field, order=1):
 
-        self.trans.fwdxTrans(field);
-        temp = self.trans.interxArr;
+        self.trans.fwdxTrans(field)
+        temp = self.trans.interxArr
 
 
-        multiplier = (self.kx)**order;
+        multiplier = (self.kx)**order
 
-        temp[:] = multiplier[np.newaxis, :] * temp[:];
-        temp[:] = self.filterX * temp[:];
+        temp[:] = multiplier[np.newaxis, :] * temp[:]
+        temp[:] = self.filterX * temp[:]
 
-        self.trans.invxTrans();
+        self.trans.invxTrans()
 
-        return self.trans.outArr.real.copy();
+        return self.trans.outArr.real.copy()
         
 
     def partialY(self,field, order=1):
 
-        self.trans.fwdyTrans(field);
-        temp = self.trans.interyArr;
+        self.trans.fwdyTrans(field)
+        temp = self.trans.interyArr
 
 
-        multiplier = (self.ky)**order;
+        multiplier = (self.ky)**order
 
-        temp[:] = multiplier[:, np.newaxis] * temp[:];
-        temp[:] = self.filterY* temp[:];
+        temp[:] = multiplier[:, np.newaxis] * temp[:]
+        temp[:] = self.filterY* temp[:]
 
-        self.trans.invyTrans();
+        self.trans.invyTrans()
 
-        return self.trans.outArr.real.copy();
+        return self.trans.outArr.real.copy()
 
     def partialChebY(self,field):
 
-        N = self.yn - 1;
-        z = cos(arange(0, N+1)*pi/N);
-        mf = arange(0, N+1);
-        m = mf[:, newaxis];
-        ans = zeros((N+1, self.xn));
+        N = self.yn - 1
+        z = cos(arange(0, N+1)*pi/N)
+        mf = arange(0, N+1)
+        m = mf[:, newaxis]
+        ans = zeros((N+1, self.xn))
 
-        newField = vstack((field, field[N-1:0:-1]));
-        self.trans.fwdyTrans(newField);
-        temp = self.trans.interCbyArr;
+        newField = vstack((field, field[N-1:0:-1]))
+        self.trans.fwdyTrans(newField)
+        temp = self.trans.interCbyArr
 
-        multiplier = 1j*m;
-        mtemp = multiplier* temp;
+        multiplier = 1j*m
+        mtemp = multiplier* temp
 
-        self.trans.invyTrans(mtemp);
-        out = self.trans.outCbyArr.real.copy();
+        self.trans.invyTrans(mtemp)
+        out = self.trans.outCbyArr.real.copy()
 
-        ans[1:N,:] = -out[1:N,:]/ (1- self.cby[1:N, np.newaxis]**2)**0.5; 
+        ans[1:N,:] = -out[1:N,:]/ (1- self.cby[1:N, np.newaxis]**2)**0.5 
 
         ans[0,:] = sum(m**2 * temp[mf,:], axis=0)/N + 0.5*N*temp[N,:];
 
@@ -108,7 +123,7 @@ class parSpectral(object):
     def ChebY(self,field):
 
         N = self.yn - 1;
-        z = cos(arange(0, N+1)*pi/N);
+        z = cos(arange(0, N+1)*pi/N) /self.lengthY;
         mf = arange(0, N+1);
         m = mf[:, newaxis];
         ans = zeros((N+1, self.xn));
@@ -129,7 +144,11 @@ class parSpectral(object):
         ans[N,:] = sum( (-1)**(m+1) * m**2 * temp[mf,:], axis=0)/N + 0.5*(-1**(N+1))* N * temp[N];
 
         return ans;
-       
+
+    def ChebMatY(self, field, order=1):
+
+        return (dot(matrix_power(self.D, order), field))
+
     def gradient(self, field):
 
        return( [self.partialX(field), self.partialY(field)]);
